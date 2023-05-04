ef9927fcd30b6ee1c4ba2de39df0b5deccebeb03
alex <alexluser101@gmail.com>
2023-05-04 22:32:39 +0530

    added a react hello-world app

---
+++ ReactJS/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <h1 className='mx-auto text-5xl flex justify-center items-center'>Hello, World!</h1>
+
+      <p className="text-xl p-4 flex justify-center">This is my First React App! Implemented with  <span className='px-1 underline underline-offset-1'>TailwindCSS</span></p>
+    </div>
+  );
+}
+
+export default App;
