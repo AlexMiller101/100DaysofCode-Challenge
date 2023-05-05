0345459456600d7fc22e9761f9f447a0c6b35805
alex <alexluser101@gmail.com>
2023-05-05 22:15:11 +0530

    added sayHello in HelloWorld.java

--- courses/Algorithm-Part-I/01-Union-Find/helloworld.java
+++ courses/Algorithm-Part-I/01-Union-Find/helloworld.java
@@ -1,6 +1,11 @@
 class HelloWorld
 {
   public static void main(String args[]) {
-    System.out.println("Hello, World!");
+    HelloWorld HelloWorld = new HelloWorld();
+    HelloWorld.sayHello("Alex");
+  }
+
+  public void sayHello(String name){
+    System.out.println("Hello " + name);
   }
 }
